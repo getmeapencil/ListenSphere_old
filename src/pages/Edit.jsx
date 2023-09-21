6e43b077e122d18440ba77566dc99297ba744f82
Sourabh <getmeapencil@gmail.com>
2023-09-21 10:22:03 +0530

    remove fontawesome and install react-icons

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,16 +1,14 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { BiSolidUser } from 'react-icons/bi';
 
 function Edit() {
     return (
       <div className='max-w-screen-xl h-screen flex flex-col items-center'>
-        <h1 className='text-4xl p-2 font-["League_Spartan"]'>ListenSphere</h1>
+        <h1 className='text-4xl pt-2 font-["League_Spartan"]'>ListenSphere</h1>
         <circle className='bg-black rounded-full w-8 h-8 flex justify-center items-center border-2 border-black'>
-        <FontAwesomeIcon icon={faUser} style={{color: "#f8a120",}} />
+          <BiSolidUser className='text-nav-yellow'/>
         </circle>
       </div>
     )
 }
   
 export default Edit;
-  
