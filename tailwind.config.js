080a922395d6b8123b9259ce3a10ce6ca8c402df
Sourabh <getmeapencil@gmail.com>
2023-09-21 09:24:27 +0530

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "label-blue": "#5CE1E6",
+        "nav-yellow": "#F8A120",
+        "button-green": "#C1FF72",
+        "button-green-dark": "#7ED957",
+        "card-grey": "#D9D9D9",
+        "card-yellow": "#FFDE59",
+        "cross-red": "#FF3131",
+        "like-blue": "#5CE1E6",
+        "card-grad-l": "#FFF7AD",
+        "card-grad-r": "#FFA9F9"
+      }
+    },
   },
   plugins: [],
 }
