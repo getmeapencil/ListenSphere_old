1e398285abe402690de5e0f16b141bd3e26499f7
Sourabh <getmeapencil@gmail.com>
2023-09-29 22:41:53 +0530

    fix navigation h1

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,7 +6,7 @@ import { BiSolidLike } from 'react-icons/bi';
 function Navigation({page}) {
     return (
         <div className="flex w-[100vw] sticky top-0 z-10 bg-white">
-            <h1 className='text-4xl py-4 font-["League_Spartan"] absolute invisible sm:visible'>ListenSphere</h1>
+            <h1 className='text-4xl py-4 font-["League_Spartan"] absolute left-6 invisible sm:visible'>ListenSphere</h1>
             <div className='flex justify-center items-center gap-4 w-full py-4'>
                 <Link to="/profile"><BiSolidUser className={`${page === "profile" ? "text-nav-yellow" : "text-white"} bg-black p-2 rounded-full text-4xl`}/></Link> 
                 <Link to="/search"><BiSolidSearch className={`${page === "search" ? "text-nav-yellow" : "text-white"} bg-black p-2 rounded-full text-4xl`}/></Link>
